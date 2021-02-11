116b164a5fbebb5a9d1b60674fe5899f1b73519b
shimanekb <brandon.j.shimanek@gmail.com>
2021-02-11 10:13:35 -0600

    added readme

--- main.go
+++ main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
+	"flag"
 	"github.com/shimanekb/project1a/controller"
 	log "github.com/sirupsen/logrus"
+	"io/ioutil"
 	"os"
 )
 
 func main() {
-	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY,
-		0666)
+	var logFlag *bool = flag.Bool("logs", false, "Enable logs")
 
-	if err != nil {
-		log.Fatalln(err)
+	if *logFlag {
+		file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY,
+			0666)
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		log.SetOutput(file)
+	} else {
+		log.SetOutput(ioutil.Discard)
 	}
-
-	log.SetOutput(file)
 
 	args := os.Args
 	if len(args) < 3 {
